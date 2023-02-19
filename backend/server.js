759768cd87cab20616d64f2561fd527b290f7f6e
ErickChezOCs <nounga72@yahoo.fr>
2023-02-19 13:52:47 +0100

    nettoyer

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    res.end('Voici la reponse du serveur à ladresse :3000!');
-});
-
-server.listen(process.env.PORT || 3000);
