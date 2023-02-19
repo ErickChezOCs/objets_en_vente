34d399043c65cd318c31504fe2290e980180e7c1
ErickChezOCs <nounga72@yahoo.fr>
2023-02-19 22:39:18 +0100

    première route créée

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 
 const app = express();
-
-app.use((req,res,next) => {
-    console.log('reception req OK!');
-    next();
+/*app.use( 'url ou end point en premier argument) l'application frontend
+fait une requête sur cette route pour recevoir une réponse contenant la
+variable stuff ici définie */
+app.use('/api/stuff',(req,res,next) => {
+    const stuff = [
+        {
+         _id :'leid1',
+         title : 'mon obje1',
+         description : 'les infos sur objet1',
+         imageUrl : 'lurl de objet1',
+         price : 4900,
+         userId : 'lidduuser'
+        },
+        {
+            _id :'leid2',
+            title : 'mon obje2',
+            description : 'les infos sur objet2',
+            imageUrl : 'lurl de objet2',
+            price : 2900,
+            userId : 'lidduuser'
+        }
+    ];
+    res.status(200).json(stuff);
 })
-app.use((req,res,next) => {
-   console.log('second:reception req OK!numero2');
-    next();
-})
-app.use((req,res,next) => {
-    res.status(201);
-     next();
- })
 
-app.use((req,res) => {
-    res.json({message:'Ceci est la troisième app.use()'});
-});
 module.exports = app;
