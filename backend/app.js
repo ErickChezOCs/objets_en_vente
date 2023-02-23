d0f25ba71794601d18e586af5d6fe3ff835544d5
ErickChezOCs <nounga72@yahoo.fr>
2023-02-23 23:52:40 +0100

    router créé

--- backend/app.js
+++ backend/app.js
@@ -22,45 +22,16 @@ app.use(express.json());
 
  
 
- app.post('/api/stuff', (req,res,next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    })
-    thing.save()
-    .then( () => res.status(201).json({ message: "Objet créé et enregistré!"}))
-    .catch(error => res.status(400).json({error}));
+ 
 
- });
 
- app.put('/api/stuff/:id',(req,res,next) =>{
-    Thing.updateOne({_id:req.params.id}, {...req.body, _id:req.params.id})
-        .then( () => res.status(200).json({message : 'Objet modifié!'}))
-        .catch(error => res.status(404).json({error}));
-});
-
-app.delete('/api/stuff/:id',(req,res,next) =>{
-    Thing.deleteOne({_id:req.params.id})
-        .then( () => res.status(200).json({message : 'Objet supprimé!'}))
-        .catch(error => res.status(404).json({error}));
-});
 
 
 /*app.use( 'url ou end point en premier argument) l'application frontend
 fait une requête sur cette route pour recevoir une réponse contenant la
 variable stuff ici définie */
-app.get('/api/stuff/:id',(req,res,next) =>{
-    Thing.findOne({_id:req.params.id})
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({error}));
-});
 
 
-app.get('/api/stuff',(req,res,next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({error}));
-});
 
 
 
