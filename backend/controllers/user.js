7194e8324dee23913a230b1eaf3497937c39a71e
ErickChezOCs <nounga72@yahoo.fr>
2023-02-26 22:12:00 +0100

    user login ok

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -18,6 +18,25 @@ exports.signup = (req,res,next) => {
 
 };
 
+exports.login = (req,res,next) => {
+    User.findOne({ email: req.body.email})
+        .then( user => {
+            if(!user) {
+                return res.sattus(401).json({ message:'Paire incohérente login/password!'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                  .then( valid => {
+                    if(!valid) {
+                        return res.status(401).json({message : 'login/password paire incohérente'});
+                    }
+                    res.status(200).json({ userId:user._id,token:'TOKEN'});
+                  })
+                  .catch(error =>res.status(500).json({error}));  
+        })
+        .catch(error => res.status(500).json({error}));
+};
+
+
 exports.login = (req,res,next ) => {
 
 };
